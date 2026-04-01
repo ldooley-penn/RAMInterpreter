950aa9e747838d3e80f190bd388d1b3b1d71ecb0
unknown <ldooley@seas.upenn.edu>
2026-04-01 19:59:49 -0400

    Add triple ram program

--- main.cpp
+++ main.cpp
@@ -381,7 +381,7 @@ int main(int argc, char* argv[]) {
         }
         catch(const std::exception& e)
         {
-            std::cerr << e.what() << '\n';
+            std::cerr << "Failed to parse line: " << line << " | Error: " << e.what() << '\n';
             bParsingFailed = true;
         }
     }
